7c29f5dfa2d964d739f3268200af180c7661d86d
georgebutler <georgebutleriv@gmail.com>
2025-01-03 14:36:26 -0800

    world

--- src/engine/world.lua
+++ src/engine/world.lua
@@ -6,12 +6,29 @@ World.__index = World
 function World.new()
 	local self = setmetatable({}, World)
 	self.tilemap = {}
+	self.seed = 1234.2
+
+	-- Scale based on tile size
+	local scale = 1 / (Config.TILES.SIZE * 2)
+
+	-- Maximum height is half of world size
+	local maxHeight = math.floor(Config.WORLD.SIZE / 2)
 
 	for x = 1, Config.WORLD.SIZE do
 		self.tilemap[x] = {}
 
+		-- Simple noise calculation
+		local noiseValue = love.math.noise(x * scale, self.seed)
+		local height = math.floor(noiseValue * maxHeight)
+
 		for y = 1, Config.WORLD.SIZE do
-			self.tilemap[x][y] = 1
+			-- Fill tiles below the height with solid blocks (1)
+			-- and leave tiles above the height empty (0)
+			if y > Config.WORLD.SIZE - height then
+				self.tilemap[x][y] = 1
+			else
+				self.tilemap[x][y] = 0
+			end
 		end
 	end
 
@@ -27,7 +44,7 @@ function World:draw()
 		for y, tile in ipairs(row) do
 			if tile == 1 then
 				love.graphics.rectangle(
-					"fill",
+					"line",
 					x * Config.TILES.SIZE,
 					y * Config.TILES.SIZE,
 					Config.TILES.SIZE,
